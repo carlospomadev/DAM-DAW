c3440d4fb0b54f1c8cd13e168f7557aadcbbf559
carlospomadev_ <c.poma.ramirez@gmail.com>
2025-03-05 18:37:54 +0100

    prueba

---
+++ PROGRAMACION/Clase2/src/Clase2/MainClase2.java
@@ -0,0 +1,14 @@
+package Clase2;
+
+public class MainClase2 {
+    public static void main(String [] arg) {
+        //Declaras una constante con la palabra final
+            final double PI = 3.1415;
+            final int mayoriaEdad = 18;
+
+        System.out.println("El vaor de PI es:" + PI);
+        System.out.println("La edad mínima para ser adutlo en Spain es:" + mayoriaEdad);
+
+    }
+
+}
